ac1cc06f2a1f64869fe2812a734c155ddacf5fa7
Danny Yoo <dyoo@cs.wpi.edu>
2011-08-10 14:50:06 -0400

    runner, but not a perfect one by any measure.

--- run.rkt
+++ run.rkt
@@ -4,17 +4,39 @@
          racket/match
          racket/path)
 
-(define handshake (make-channel))
-(define t 
- (thread (lambda ()
-           (define receiver (make-log-receiver (current-logger) 'debug))
-           (define (loop)
-             (displayln (sync receiver))
-             (loop))
-           (channel-get handshake)
-           (loop))))
+(define cmd-channel (make-channel))
 
-(channel-put handshake 'program-done)
+(define (request/response cmd)
+  (channel-put cmd-channel cmd)
+  (channel-get cmd-channel))
+
+(define-struct print-cmd (v))
+
+(define t 
+  (thread (lambda ()
+            (let/ec break
+              (let loop ()
+                (sync (handle-evt cmd-channel
+                                  (lambda (cmd)
+                                    (cond 
+                                      [(print-cmd? cmd)
+                                       (displayln (print-cmd-v cmd))
+                                       (channel-put cmd-channel 'ok)]
+                                      [(eq? cmd 'ready?)
+                                       (channel-put cmd-channel 'ok)]
+                                      [(eq? cmd 'quit!)
+                                       (channel-put cmd-channel 'ok)
+                                       (break)]))))
+                (loop))))))
+                                   
+
+(void (thread (lambda ()
+                (define receiver (make-log-receiver (current-logger) 'debug))
+                (let loop ()
+                  (request/response (make-print-cmd (sync receiver)))
+                  (loop)))))
+
+
 (parameterize ([read-accept-reader #t]
                [current-namespace (make-base-namespace)])
   (match (current-command-line-arguments)
@@ -24,14 +46,23 @@
     [(vector program)
      (define-values (base name dir?)
        (split-path (normalize-path program)))
+     (define module-name (string->symbol
+                          (regexp-replace #px"\\.\\w+$" 
+                                          (path->string name) 
+                                          "")))
      (parameterize ([current-directory base]
                     [current-load-relative-directory base])
        (define prog-stx 
          (call-with-input-file name 
            (lambda (ip)
              (read-syntax #f ip))))                  
-
+       (request/response 'ready?)
+       
        (eval (annotate-applications prog-stx))
-       (dynamic-require ''foo #f))]))
+       (dynamic-require `',module-name #f))]))
 
+
+(printf "Press enter to quit.\n")
+(void (read-line))
+(void (request/response 'quit!))
 (thread-wait t)
